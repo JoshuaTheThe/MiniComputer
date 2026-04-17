05421911558eace4c30ee9a5d643c7a723ffa2b1
Joshua Farmer <rhubarbjosh@outlook.com>
2026-04-17 17:32:31 +0100

    simple A-Z program

--- a.s
+++ a.s
@@ -1,5 +1,12 @@
-        LEA     0x3FFF0
-        TXA
-        LEA     0x41
-        STA     IX,0x00
-        HLT
+        LEA     65
+        STA     0
+        LDA     0
+        STA     0x3fff0
+        LDA     0
+        LEA     1
+        ADC     0
+        STA     0
+        LEA     91
+        SBB     0
+        HLT,Z
+        JMP     4
