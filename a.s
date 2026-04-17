168e5e98d7d1dbeb0f0744dc56abc866f360c117
Joshua Farmer <rhubarbjosh@outlook.com>
2026-04-17 17:34:06 +0100

    changed addrress in example

--- a.s
+++ a.s
@@ -1,12 +1,12 @@
         LEA     65
-        STA     0
-        LDA     0
+        STA     256
+        LDA     256
         STA     0x3fff0
-        LDA     0
+        LDA     256
         LEA     1
-        ADC     0
-        STA     0
+        ADC     256
+        STA     256
         LEA     91
-        SBB     0
+        SBB     256
         HLT,Z
         JMP     4
